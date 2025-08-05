a72f6512c3df844d778a0c7a9859f554d28ac84e
Bruna da Silva Donati <brunadasilvadonati@gmail.com>
2025-08-05 15:30:17 -0300

    criado componente do footer

---
+++ páginasReact/site-React/src/componentes/footer.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+import whatsappIcon from '../img/whatsapp.png';
+import linkedinIcon from '../img/linkedin.png';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="elementos1">
+        <h3>Renassol</h3>
+        <div className="icones">
+          <img src={whatsappIcon} alt="whatsapp" />
+          <img src={linkedinIcon} alt="linkedin" />
+        </div>
+      </div>
+
+      <div className="coluna1">
+        <Link to="/nossa-empresa">Nossa empresa</Link>
+        <Link to="/importancia">Importância</Link>
+        <Link to="/opcoes">Opções</Link>
+        <Link to="/componentes">Componentes</Link>
+        <Link to="/descarte">Descarte</Link>
+      </div>
+
+      <div className="coluna2">
+        <Link to="/cadastro">Cadastro</Link>
+        <Link to="/loja">Loja</Link>
+        <Link to="/carrinho">Carrinho</Link>
+        <Link to="/finalizar-compra">Finalizar compra</Link>
+        <Link to="/api">API</Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

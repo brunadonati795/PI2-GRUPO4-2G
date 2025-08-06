2a8073f5dbf11875786a39ac2ebf5a52ad054f4a
Luísa <luisa.baggio16@gmail.com>
2025-08-06 14:02:01 -0300

    Create descarte.jsx
    transformação pra react, da página de descarte

---
+++ páginasReact/site-React/src/páginas/descarte.jsx
@@ -0,0 +1,31 @@
+
+const App = () => {
+  return (
+    <div className="Descarte">
+      <h1 className="titulo">Descarte de Placas Solares</h1>
+
+      <div className="info-container">
+        <div className="texto">
+          <p>
+            Nós, da Renassol, ajudamos você a descartar suas placas solares e realizamos sua reciclagem,
+            garantindo o destino correto para elas.<br /><br />
+            Informe-nos seu endereço para buscarmos suas placas.
+          </p>
+        </div>
+
+        <div className="imagem">
+          <img
+            src="../imagens/descarte.png"
+            alt="descarte"
+          />
+        </div>
+      </div>
+
+      <div className="botao-formulario">
+        <button className="btn-formulario">Acessar Formulário</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;

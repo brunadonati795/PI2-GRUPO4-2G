0bb663965ca91243c55f039b8e2e73e4e54fa820
Sexo <karinejohann53@gmail.com>
2025-08-13 15:41:24 -0300

    algumas modificações no carrinho

--- páginas/Carrinho/script.js
+++ páginas/Carrinho/script.js
@@ -4,8 +4,10 @@ const carrinho = document.getElementById('carrinho');
 
 abrirCarrinho.addEventListener('click', () => {
   carrinho.style.right = '0';
+  abrirCarrinho.style.display = 'none'; // esconde a imagem
 });
 
 fecharCarrinho.addEventListener('click', () => {
   carrinho.style.right = '-50vw';
+  abrirCarrinho.style.display = 'block'; // mostra a imagem novamente
 });

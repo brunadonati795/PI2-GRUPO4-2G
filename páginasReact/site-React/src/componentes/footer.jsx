653842cb54c0e1f8daa53f0502b292c3efc657f1
pedrofreitasz <162584971+pedrofreitasz@users.noreply.github.com>
2025-08-27 14:36:03 -0300

    responsividade do footer

--- páginasReact/site-React/src/componentes/footer.jsx
+++ páginasReact/site-React/src/componentes/footer.jsx
@@ -1,28 +1,27 @@
 import { Link } from 'react-router-dom';
 import "../App.css";
-import logoAmarela from '../assets/imagens/logoAmarela.png'
+import logoAmarela from '../assets/imagens/logoAmarela.png';
 import Funcionamento from "../páginas/funcionamento";
 import Suporte from "../páginas/suporte";
 import Descarte from "../páginas/descarte";
 import Opcoes from "../páginas/opcoes.jsx";
-// import Componentes from "../páginas/componentes.jsx";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="elementos1">
-        <h3 className='footer-titulo'>RENASSOL</h3>
-        <img src={logoAmarela} className='footerimg'/>
+    <footer className="ftr_container">
+      <div className="ftr_blocoPrincipal">
+        <h3 className='ftr_titulo'>RENASSOL</h3>
+        <img src={logoAmarela} className='ftr_logo' alt="Logo" />
       </div>
 
-      <div className="coluna1">
+      <div className="ftr_colunaUm">
         <Link to="/nossa-empresa">Nossa empresa</Link>
         <Link to="/opcoes">Opções</Link>
         <Link to="/componentes">Componentes</Link>
         <Link to="/descarte">Descarte</Link>
       </div>
 
-      <div className="coluna2">
+      <div className="ftr_colunaDois">
         <Link to="/cadastro">Cadastro</Link>
         <Link to="/loja">Loja</Link>
         <Link to="/carrinho">Carrinho</Link>
